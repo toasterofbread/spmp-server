030ecda4be429273c4c6367f34de0914874addea
toasterofbread <talohalton@gmail.com>
2024-11-24 12:49:44 +0000

    .

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -43,7 +43,7 @@ kotlin {
 
     kjna {
         generate {
-            include_dirs += listOf(current_platform.getNativeDependenciesDir(project).resolve("include").absolutePath.replace("\\", "/"))
+            include_dirs += listOf(current_platform.getNativeDependenciesDir(project).resolve("include").safeAbsolutePath)
             parser_include_dirs += listOf("/usr/include/linux/", "/usr/lib/gcc/x86_64-pc-linux-gnu/14.1.1/include/")
 
             packages(native_targets) {
@@ -362,7 +362,7 @@ enum class CinteropLibraries {
 
         def_file.writeText("""
             staticLibraries = ${lib_filenames.joinToString(" ")}
-            libraryPaths = ${lib_dirs.map { it.absolutePath }.joinToString(" ")}
+            libraryPaths = ${lib_dirs.map { it.safeAbsolutePath }.joinToString(" ")}
             linkerOpts = ${linker_opts.joinToString(" ")}
         """.trimIndent())
 
@@ -549,3 +549,6 @@ object Static {
         return name
     }
 }
+
+val File.safeAbsolutePath: String
+    get() = absolutePath.replace("\\", "/")
